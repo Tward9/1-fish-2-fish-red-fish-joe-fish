e2df22101fc8e7577e7637a496b96405899a388b
Andrew Moody <77068588+andrewmoody96@users.noreply.github.com>
2022-03-08 22:42:25 -0600

    HTML reflects wireframe. Tailwind CSS implemented. JS is successfully pulling from fish API.
    Co-authored-by: molee08 <molee08@users.noreply.github.com>
    Co-authored-by: Theo Ward <theoward9@gmail.com>
    Co-authored-by: Jessica Parker <jlparker3@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,38 @@
+//url for fishwatch api
+//can set to update with specific species in the future
+var fishURL = "https://cors-anywhere-bc.herokuapp.com/https://www.fishwatch.gov/api/species";
+var redSnapperURL = "https://cors-anywhere-bc.herokuapp.com/https://www.fishwatch.gov/api/species/red-snapper";
+var weather = 
+
+//querry fishwatch api
+function getFishAPI() {
+  fetch(fishURL)
+    .then(function (response) {
+      console.log("fetched");
+      return response.json();
+    })
+    .then(function (fishData) {
+      console.log(fishData);
+      for (i = 0; i < fishData.length; i++) {
+        console.log(fishData[i].Location);
+        console.log(fishData[i]['Species Name']);
+        //need to fix addition to table just for data display
+        $('#fishTable').append(`<tr><td>` +$(fishData[i].Location)+`</td><td>` +$(fishData[i]['Species Name'])+`</td></tr>`)
+      }
+    });
+}
+
+function getredSnapperAPI() {
+  fetch(redSnapperURL)
+    .then(function (snapperResponse) {
+      console.log("fetched snapper");
+      return snapperResponse.json();
+    })
+    .then(function (redSnapperData) {
+      console.log(redSnapperData);
+      console.log(redSnapperData.Location);
+    });
+}
+
+$("#fishBtn").on("click", getFishAPI);
+$("#redSnapperBtn").on("click", getredSnapperAPI);
