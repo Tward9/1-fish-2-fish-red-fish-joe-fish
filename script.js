f1974852eb1495f0fa2ac0f507a17930e91467d3
Andrew Moody <77068588+andrewmoody96@users.noreply.github.com>
2022-03-12 00:16:36 -0600

    API fetches all required data. Appends 3 separate temps to the same block.

--- script.js
+++ script.js
@@ -3,22 +3,25 @@
 var fishURL = "https://cors-anywhere-bc.herokuapp.com/https://www.fishwatch.gov/api/species";
 var redSnapperURL = "https://cors-anywhere-bc.herokuapp.com/https://www.fishwatch.gov/api/species/red-snapper";
 
-// Weather API Info
-var cityName = "Chicago";
-// create separate input for statecode
-var state = "IL";
+// WEATHER VARIABLES:
+// ---------------------------------------------
+// location:
+var cityName = "";
+var state = "";
 var countryCode = ",US";
 var locationURL = "http://api.openweathermap.org/geo/1.0/direct?q=";
 var APIkeyLocation = "306e5a39201f9a04bf59daf2b8544d8a";
-var weatherURL = "https://cors-anywhere-bc.herokuapp.com/http://api.weatherunlocked.com/api/current/";
-var APIkeyWeather = "app_key=653627bc29b26515ff93a6d98e85de58";
-var appIDweather = "app_id=78fab2ab&";
+// weather:
+var weatherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=";
+var APIkeyWeather = "c443fd962d906a14e3af8d9d37623de2";
 var picCurrent = $(`<img>`);
 var picDay2 = $(`<img>`);
 var picDay3 = $(`<img>`);
+var weatherList = $(`<ul>`);
 
 
-function getLocation(){
+function displayWeather(){
+  // Gets location from user input and converts to latitude & longitude.
   cityName = $("#userSearch").val();
   state = $("#userState").val()
   fetch(locationURL + cityName + "," + state + countryCode + "&limit=1&appid=" + APIkeyLocation)
@@ -35,23 +38,79 @@ function getLocation(){
     
     getWeather(lat, lon);
     
+    // Uses latitude & longitude values to get a forecast for the location.
     function getWeather() {
-      fetch(weatherURL + lat + "," +lon + "?" + appIDweather + APIkeyWeather)
+      var weatherIcon = `http://openweathermap.org/img/wn/`
+      fetch(weatherURL + lat + "&lon=" + lon + "&exclude=minutely,hourly&units=imperial&lang=en&appid=" + APIkeyWeather)
       .then(function (response) {
         console.log("getting weather...")
         return response.json();
       })
-      .then(function (weatherData) {
-        // Append this info to weather dayBlocks
-        console.log(weatherData.wx_icon);
-        let weatherIMG = weatherData.wx_icon
-        picCurrent = $(picCurrent).attr("src", `./Assets/Images/WeatherUnlocked/${weatherIMG}`);
-        $(picCurrent).appendTo("#day1");
-        console.log(weatherData.temp_f);
-        console.log(weatherData.feelslike_f);
-        console.log(weatherData.humid_pct);
-        console.log(weatherData.windspd_mph);
-        console.log(weatherData.winddir_compass);
+      .then(function (weatherResponse) {
+        // Retrieves and appends weather data from the first 3 days of the forecast.
+        for (var i = 0; i < 3; i++) {
+          var weatherIMG = weatherResponse.daily[i].weather[0].icon;         
+          var tempHigh = weatherResponse.daily[i].temp.max;
+          var tempLow = weatherResponse.daily[i].temp.min;
+          var conditions = weatherResponse.daily[i].weather[0].description;
+          var humidity = weatherResponse.daily[i].humidity;
+          var windSpeed = weatherResponse.daily[i].wind_speed;
+          var windDegree = weatherResponse.daily[i].wind_deg;
+          var listData = $(`<li>`)
+
+          if (i === 0) {
+            console.log("1");
+            // retrieves weather icon & appends to weather block
+            picCurrent =$(picCurrent).attr("src", `${weatherIcon}${weatherIMG}.png`);
+            $(picCurrent).appendTo("#day1")
+
+            // creates & appends items to list
+            weatherList = $(weatherList).attr("id", "day1data")
+            $(weatherList).appendTo("#day1");
+
+            listData = $(listData).text(tempHigh);
+            $(listData).appendTo("#day1data");
+
+            // listData = $(listData).text(tempLow);
+            // $(listData).appendTo("#day1data");
+
+            // listData = $(listData).val(conditions);
+            // $(listData).appendTo("#day1data");
+
+            // listData = $(listData).val(humidity);
+            // $(listData).appendTo("#day1data");
+
+            // listData = $(listData).val(windSpeed);
+            // $(listData).appendTo("#day1data");
+
+            // listData = $(listData).val(windDegree);
+            // $(listData).appendTo("#day1data"); 
+          }
+          if (i === 1) {
+            console.log("2");
+            picCurrent =$(picDay2).attr("src", `${weatherIcon}${weatherIMG}.png`);
+            $(picCurrent).appendTo("#day2")
+            
+            // creates & appends items to list
+            weatherList = $(weatherList).attr("id", "day2data")
+            $(weatherList).appendTo("#day2");
+
+            listData = $(listData).text(tempHigh);
+            $(listData).appendTo("#day2data");
+          }
+          if (i === 2) {
+            console.log("3");
+            picCurrent =$(picDay3).attr("src", `${weatherIcon}${weatherIMG}.png`);
+            $(picCurrent).appendTo("#day3")
+            
+            // creates & appends items to list
+            weatherList = $(weatherList).attr("id", "day3data")
+            $(weatherList).appendTo("#day3");
+
+            listData = $(listData).text(tempHigh);
+            $(listData).appendTo("#day3data");
+          }
+        }
       })
     };
   })
@@ -61,5 +120,5 @@ function getLocation(){
 };
 
 
-$("#fishBtn").on("click", getLocation);
+$("#fishBtn").on("click", displayWeather);
 
