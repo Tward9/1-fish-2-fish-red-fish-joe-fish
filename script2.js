9e79d2973270838039f326fb9e86c87f200ce93b
Tward9 <97201650+Tward9@users.noreply.github.com>
2022-03-11 19:06:29 -0600

    fixed modal opening, opens on all buttons

--- script2.js
+++ script2.js
@@ -13,31 +13,34 @@ function getFishAPI() {
             $('#regionList').empty();
             $('#btnList').empty();
             console.log(fishData);
-            console.log(fishData[0]['Species Name']);
-            var i = 0;
-            fishData.forEach(fish => {
+            // console.log(fishData[0]['Species Name']);
+            fishData.forEach(function (fish, i) {
+                // console.log(i);
                 //id shows as "item_undefined_n"
-                $('#nameList').append($('<li/>').attr("id", ('item_' + i + '_n')).text(fish['Species Name']));
-                $('#regionList').append($('<li/>').attr("id", 'item_' + i + '_r').text(fish['NOAA Fisheries Region']));
-                $('#btnList').append($('<li/>').attr("id", 'item_' + i + '_btn', 'class', 'modal_open').append($('<button/>',
-                    { text: 'Fun ' + fish['Species Name'] + ' Facts', id: 'btn_' + i, class: 'modal-open' })));
-                // $('#btn_' + i).on('click', function (event) {
+                $('#nameList').append($('<li/>').attr("id", 'item_' + i + '_n').addClass('item_' + i).text(fish['Species Name']));
+                $('#regionList').append($('<li/>').attr("id", 'item_' + i + '_r').addClass('item_' + i).text(fish['NOAA Fisheries Region']));
+                $('#btnList').append($('<li/>').attr("id", 'item_' + i + '_btn', 'class', 'modal_open', 'item_' + i).append($('<button/>',
+                    { text: 'Fun ' + fish['Species Name'] + ' Facts', id: 'btn_' + i, class: 'modal-open' }).addClass('item_' + i)));
+                // $('table').addClass('rounded-lg p-2 text-xl bg-emerald-300 border-solid border-2 border-gray-900');
+                // $('button').addClass('rounded-lg p-1 text-l bg-emerald-300 border-solid border-2 border-gray-900');
 
                 // })
-                //modal opening on every other button for some reason
-                var openmodal = document.querySelectorAll('.modal-open')
-                for (var i = 0; i < openmodal.length; i++) {
-                    openmodal[i].addEventListener('click', function (event) {
-                        // event.preventDefault()
-                        toggleModal()
-                    })
-                }
-                const overlay = document.querySelector('.modal-overlay')
-                overlay.addEventListener('click', toggleModal)
-                var closemodal = document.querySelectorAll('.modal-close')
-                for (var i = 0; i < closemodal.length; i++) {
-                    closemodal[i].addEventListener('click', toggleModal)
-                }
+
+                $('.modal-open').on('click', function (event) {
+                    event.stopImmediatePropagation();
+                    event.preventDefault();
+                    console.log('clicked');
+                    var clickedBtnClass = event.target.className;
+                    console.log(clickedBtnClass);
+                    //needs to link id/class of items for information to be able to add to modals
+                    toggleModal();
+                })
+
+                const overlay = document.querySelector('.modal-overlay');
+                overlay.addEventListener('click', toggleModal());
+                var closemodal = $('.modal-close');
+                closemodal.on('click', toggleModal());
+
                 document.onkeydown = function (evt) {
                     evt = evt || window.event
                     var isEscape = false
@@ -57,7 +60,6 @@ function getFishAPI() {
                     modal.classList.toggle('pointer-events-none')
                     body.classList.toggle('modal-active')
                 }
-                i++;
                 //button creates modal with info from fish api
                 //display fish image
             });
